e67e5cb3be684571381fa5edf2a74eaa426083c1
Kenji <fcb141021@yahoo.co.jp>
2023-08-01 13:19:14 +0900

    つぶやき投稿機能の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,17 @@ class PostsController < ApplicationController
   end
 
   def new
-    
+    @post = Post.new
+  end
+
+  def create
+    Post.create(blog_params)
+    redirect_to new_post_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:post).permit(:content)
   end
 end
